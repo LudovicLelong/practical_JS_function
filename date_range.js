151e63899a42f75911b9f4d5fe4b44e241becdad
Ludovic <ludoviclelong10@gmail.com>
2023-03-06 14:39:50 +0100

    Date range functions

---
+++ date_range.js
@@ -0,0 +1,39 @@
+const YEAR = 0
+const MONTHS = 1
+const DAYS = 2
+const HOURS = 3
+const MINUTES = 4
+const SECONDS = 5
+
+
+/**
+ * 
+ * @param {Date} date 
+ * @param {Object} interval 
+ * @returns 
+ */
+const addInterval = function(date, interval) {
+    const parts = [
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+        date.getMilliseconds()
+    ]
+    for (const [unit, value] of Object.entries(interval)) {
+        parts[unit] += value
+    }
+    return new Date(...parts)
+}
+
+const today = new Date();
+const future = addInterval(today, { 
+    [MONTHS]: 3,
+    [DAYS]: 1,
+    [MINUTES]: 3,
+})
+
+console.log(today)
+console.log(future)
